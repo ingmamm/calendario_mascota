4d6453f5b630c24d786e03daabf9480a77a28b2f
griffith1989 <ndpaez89@gmail.com>
2014-11-23 15:37:05 -0400

    Nico
    Agregado datos para pruebas tabla usuario y mascota

--- src/com/example/calendario_mascota/db/Calendario_MascotasDBOpenHelper.java
+++ src/com/example/calendario_mascota/db/Calendario_MascotasDBOpenHelper.java
@@ -30,6 +30,16 @@ public class Calendario_MascotasDBOpenHelper extends SQLiteOpenHelper {
 			"descripcion TEXT," +
 			"fecha TEXT)";
 	
+	private static final String INSERTAR_USUARIO1 = "INSERT INTO usuario" +
+	"(user, pass, logueado) VALUES ('chango', '1234', 'false')";
+	
+	private static final String INSERTAR_MASCOTA1 = "INSERT INTO mascota" +
+			"(id_usuario, nombre, tipo, raza, fecha_nac) VALUES (" +
+			"'1', 'pushol', 'perro', 'siberiano'. '13-13-13')";
+	
+	private static final String INSERTAR_MASCOTA2 = "INSERT INTO mascota" +
+			"(id_usuario, nombre, tipo, raza, fecha_nac) VALUES (" +
+			"'1', 'colita', 'perro', 'golden retriever'. '13-13-13')";
 	
 	private static final String INSERTAR_EVENTO1 = "INSERT INTO evento " +
 			"(id_mascota, tipo, descripcion, fecha) VALUES (" +
@@ -37,11 +47,16 @@ public class Calendario_MascotasDBOpenHelper extends SQLiteOpenHelper {
 	
 	private static final String INSERTAR_EVENTO2 = "INSERT INTO evento " +
 			"(id_mascota, tipo, descripcion, fecha) VALUES (" +
-			"'2','chango', 'hola que tal esto es una prueba', '13-13-13')";
+			"'1','chango', 'hola que tal esto es una prueba', '13-13-13')";
 	
 	private static final String INSERTAR_EVENTO3 = "INSERT INTO evento " +
 			"(id_mascota, tipo, descripcion, fecha) VALUES (" +
-			"'3','chango', 'hola que tal esto es una prueba', '13-13-13')";
+			"'1','chango', 'hola que tal esto es una prueba', '13-13-13')";
+	
+	private static final String INSERTAR_EVENTO4 = "INSERT INTO evento " +
+			"(id_mascota, tipo, descripcion, fecha) VALUES (" +
+			"'2','chango', 'hola que tal esto es una prueba', '13-13-13')";
+	
 	
 	public Calendario_MascotasDBOpenHelper (Context context){
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -56,6 +71,10 @@ public class Calendario_MascotasDBOpenHelper extends SQLiteOpenHelper {
 		db.execSQL(INSERTAR_EVENTO1);
 		db.execSQL(INSERTAR_EVENTO2);
 		db.execSQL(INSERTAR_EVENTO3);
+		db.execSQL(INSERTAR_EVENTO4);
+		db.execSQL(INSERTAR_MASCOTA1);
+		db.execSQL(INSERTAR_MASCOTA2);
+		db.execSQL(INSERTAR_USUARIO1);
 	}
 
 	@Override
